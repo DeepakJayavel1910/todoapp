9d32dd42ce4af66ba5f3560c2d2bde2af3ed9b87
Deepak <jayaveldeepak19@gmail.com>
2024-10-14 23:00:45 +0530

    vercel.json added

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,7 @@ app.get("/",(req,res)=>{
 })
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
-app.use(cors({
-    origin: "https://todo-backend-6l76.onrender.com",
-}));
+app.use(cors());
 // Middleware
 app.use(express.json());
 app.use('/todos', todoRoutes);
