45130b5d4db9b8596046a62e034e1ac3470c6c36
Deepak <jayaveldeepak19@gmail.com>
2024-10-14 21:34:33 +0530

    Second Commit

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const todoRoutes = require('./routes/todoRoutes');
 require('dotenv').config();
 
-//username : jayaveldeepak19
-//pssword : FGnvFzw9uXPS3pxW
 
-//url:
 
 const app = express();
+app.get("/",(req,res)=>{
+    res.send("It is a todoapp Backend service - /todos to acess the todos json datas")
+})
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    origin: "https://todo-backend-6l76.onrender.com",
+}));
 // Middleware
 app.use(express.json());
 app.use('/todos', todoRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+const apiUrl = import.meta.env.APIURL;
 
 const App = () => {
     const [todos, setTodos] = useState([]);
-
     useEffect(() => {
         const fetchTodos = async () => {
-            const response = await axios.get('http://localhost:5000/todos');
+            const response = await axios.get(`${apiUrl}/todos`);
             setTodos(response.data);
         };
         fetchTodos();
@@ -16,7 +16,7 @@ const App = () => {
 
     const addTodo = async (newTodo) => {
         try {
-            const response = await axios.post('http://localhost:5000/todos', newTodo);
+            const response = await axios.post(`${apiUrl}/todos`, newTodo);
             setTodos([...todos, response.data]);
         } catch (error) {
             console.error('Error adding todo:', error);
@@ -25,7 +25,7 @@ const App = () => {
 
     const deleteTodo = async (id) => {
         try {
-            await axios.delete(`http://localhost:5000/todos/${id}`);
+            await axios.delete(`${apiUrl}/todos/${id}`);
             setTodos(todos.filter(todo => todo._id !== id));
         } catch (error) {
             console.error('Error deleting todo:', error);
@@ -34,7 +34,7 @@ const App = () => {
 
     const toggleComplete = async (id, completed) => {
         try {
-            const response = await axios.patch(`http://localhost:5000/todos/${id}`, { completed: !completed });
+            const response = await axios.patch(`${apiUrl}/todos/${id}`, { completed: !completed });
             setTodos(todos.map(todo => (todo._id === id ? response.data : todo)));
         } catch (error) {
             console.error('Error updating todo:', error);
