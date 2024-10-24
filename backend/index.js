ccf829239164043fdab1acae6099e33cc9e1fc63
Deepak <jayaveldeepak19@gmail.com>
2024-10-24 14:27:49 +0530

    origin fixed

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://todoapp-olive-phi.vercel.app/"],
+    origin: ["https://todoapp-olive-phi.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
