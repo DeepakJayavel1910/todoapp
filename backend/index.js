5fecb227b7d8f2cc9c5fa704bb40c759ae13cff0
Deepak <jayaveldeepak19@gmail.com>
2024-10-26 11:19:33 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     origin: ["https://todoapp-olive-phi.vercel.app"],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "DELETE", "PATCH"],
     credentials: true,
   })
 );
